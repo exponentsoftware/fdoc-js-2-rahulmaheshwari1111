e4e6d4a376f46e04aff2374a72ad3d4a21d4e385
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-09-19 14:08:39 +0100

    2nd ques i part

---
+++ q2.js
@@ -0,0 +1,58 @@
+// Write a function which filter users who has scoresGreaterThan85,
+// Write a function which addUser to the user array only if the user does not exist.
+// Write a function which addUserSkill which can add skill to a user only if the user exist.
+// Write a function which editUser if the user exist in the users array.
+const users = [
+{
+	name:'Brook', 
+	scores:75,
+	skills:['HTM', 'CSS', 'JS'],
+	age:16
+},
+{
+	name:'Alex', 
+	scores:80,
+	skills:['HTM', 'CSS', 'JS'],
+	age:18
+}, 
+{
+	name:'David', 
+	scores:75,
+	skills:['HTM', 'CSS'],
+	age:22
+}, 
+{
+	name:'John', 
+	scores:85,
+	skills:['HTM'],
+	age:25
+},
+{
+	name:'Sara',
+	scores:95,
+	skills:['HTM', 'CSS', 'JS'],
+	age: 26
+},
+{
+	name:'Martha', 
+	scores:80,
+	skills:['HTM', 'CSS', 'JS'],
+	age:18
+},
+{
+	name:'Thomas',
+	scores:90,
+	skills:['HTM', 'CSS', 'JS'],
+	age:20
+}
+];
+
+const scoresGreaterThan85 = ()=>{
+  const result=  users.filter((newUser)=> {
+     return newUser.scores > 85
+    })
+    console.log(result)
+}
+scoresGreaterThan85()
+
+
